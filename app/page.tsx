9d131637f1fb9fb5395e0e6635620f03ac255546
Paul Davis <paulcdavis135@gmail.com>
2023-07-11 22:33:06 -0400

    Testing with button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+  const [count, setCount] = useState(0)
+  
   return (
     <>
       <h1>Paul{"'"}s Portfolio Website</h1>
       <p>This page is static. It was built on 06/09/2023.</p>
       <p>Adding a new commit to test if it updates</p>
+      <p>This is how many clicks: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
     </>
   );
 }
